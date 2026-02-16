789ee222175215ea0c75e3998f0010097c75f8c1
Ibukun Adepoju-Conde <ibeksconde@gmail.com>
2026-02-16 15:05:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+// Task 6.2: Import the Provider component from the react-redux library
+import { Provider } from 'react-redux' 
+// Task 6.3: Import the Redux store configured in store.js
+import store from './store.js' 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Task 6.4: Wrap the App component with the Provider and pass the store as a prop */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
