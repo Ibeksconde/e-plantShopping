fbbea4615eb58ca5840e0dcd99a40a9cd1d50b39
Ibukun Adepoju-Conde <ibeksconde@gmail.com>
2026-02-16 15:02:48 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+// Task 5: Configure the Redux store
+const store = configureStore({
     reducer: {
+        // 'cart' is the name of the slice in the store, managed by cartReducer
         cart: cartReducer,
     },
 });
-export default store
+
+// Task 5: Export the store for use in the application
+export default store;
